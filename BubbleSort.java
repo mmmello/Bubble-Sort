ef9efdec93f9bc60d4e97afa53aca8236e3aac52
mmmello <matheusmello142012@gmail.com>
2022-02-18 01:31:34 -0300

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.util.Random;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		Random generator = new Random();
+		int v[] = new int[10];
+		
+		// Filling the vector
+		for(int i=0; i<v.length; i++) v[i] = generator.nextInt(26);
+		
+		// Showing the vector before sorting
+		System.out.printf("Out of order: ");
+		for(int i=0; i < v.length; i++) System.out.printf("%d ", v[i]);
+		
+		bubbleSort(v);
+		
+		// Showing the vector after sorting 
+		System.out.printf("\n\nSorted: ");
+		for(int i=0; i < v.length; i++) System.out.printf("%d ", v[i]);
+	}
+	
+	public static void bubbleSort(int v[]){
+		int aux, end = v.length;
+		boolean change = true;
+		
+		while(change==true){
+			change = false;
+			end--;
+			for(int i=0 ; i < end; i++){
+
+				// The operator turned to the right puts the vector in ascending order
+				// The operator facing left puts the vector in descending order
+				if(v[i]>v[i+1]){
+					aux = v[i];
+					v[i] = v[i+1];
+					v[i+1] = aux;
+					change = true;
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## Bubble Sort
+
+This is a simple project to sort vectors using the Bubble Sort method.
+
+Bubble sort is the simplest but least efficient algorithm in comparison to Insertion Sort and Selection Sort (both methods are covered in two repositories in my profile). In this algorithm each element of position i will be compared with its successor, i.e. an element of position 2 will be compared with the element of position 3. If the element of position 2 is larger than the element of position 3 they swap places, and so on. Because of this way of execution, the vector will have to be traversed as many times as necessary, making the algorithm very repetitive and inefficient. And when we have a large list, the problem is even bigger.
+
+The programming language used in this algorithm will be java.
